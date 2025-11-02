a11cd72b07adf820a0cbabc02fc346cece84809f
Lv1g1 <luigi.inguaggiato@gmail.com>
2025-11-02 20:31:34 +0100

    try adding hyper tuning with optuna

--- Challenge/hyper_tuning.py
+++ Challenge/hyper_tuning.py
@@ -1,32 +1,21 @@
-import paths
-
 import pandas as pd
-import scipy.sparse as sps
-from Recommenders.KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
-from Evaluation.Evaluator import EvaluatorHoldout
 import optuna
 
-# Load data
-URM_train = sps.load_npz(paths.URM_TRAIN)
-URM_validation = sps.load_npz(paths.URM_VALIDATION)
-
-# Initialize evaluator
-evaluator_validation = EvaluatorHoldout(URM_validation, [10])
-
 # Objective function that will be run by the optimizer
+# Example implementation:
+"""
 def objective_function(optuna_trial):
-    
     recommender_instance = ItemKNNCFRecommender(URM_train)
     recommender_instance.fit(topK = optuna_trial.suggest_int("topK", 5, 1000),
-                             shrink = optuna_trial.suggest_int("shrink", 0, 1000),
-                             similarity = "cosine",
-                             normalize = optuna_trial.suggest_categorical("normalize", [True, False])
+                            shrink = optuna_trial.suggest_int("shrink", 0, 1000),
+                            similarity = "cosine",
+                            normalize = optuna_trial.suggest_categorical("normalize", [True, False])
                             )
     
     result_df, _ = evaluator_validation.evaluateRecommender(recommender_instance)
     
     return result_df.loc[10]["MAP"]
-
+"""
 
 # Callback class to save results
 class SaveResults(object):
@@ -40,32 +29,32 @@ class SaveResults(object):
         
         self.results_df = self.results_df.append(hyperparam_dict, ignore_index=True)
 
-# Run optimization
-optuna_study = optuna.create_study(direction="maximize")
-        
-save_results = SaveResults()
-        
-optuna_study.optimize(objective_function,
-                      callbacks=[save_results],
-                      n_trials = 50)
+# Function to perform hyperparameter tuning
+# Takes an objective function as input
+def hyperparameter_tuning(objective_function, n_trials):
+    # Run optimization
+    optuna_study = optuna.create_study(direction="maximize")
+            
+    save_results = SaveResults()
+            
+    optuna_study.optimize(objective_function,
+                        callbacks=[save_results],
+                        n_trials = n_trials)
 
+    # Print study results
+    pruned_trials = [t for t in optuna_study.trials if t.state == optuna.trial.TrialState.PRUNED]
+    complete_trials = [t for t in optuna_study.trials if t.state == optuna.trial.TrialState.COMPLETE]
 
-# Print study results
-pruned_trials = [t for t in optuna_study.trials if t.state == optuna.trial.TrialState.PRUNED]
-complete_trials = [t for t in optuna_study.trials if t.state == optuna.trial.TrialState.COMPLETE]
+    print("Study statistics: ")
+    print("  Number of finished trials: ", len(optuna_study.trials))
+    print("  Number of pruned trials: ", len(pruned_trials))
+    print("  Number of complete trials: ", len(complete_trials))
 
-print("Study statistics: ")
-print("  Number of finished trials: ", len(optuna_study.trials))
-print("  Number of pruned trials: ", len(pruned_trials))
-print("  Number of complete trials: ", len(complete_trials))
+    print("Best trial:")
+    print("  Value Validation: ", optuna_study.best_trial.value)
 
-print("Best trial:")
-print("  Value Validation: ", optuna_study.best_trial.value)
+    print("  Params: ", optuna_study.best_trial.params)
+    print("All results:")
+    print(save_results.results_df)
 
-print("  Params: ", optuna_study.best_trial.params)
-print("All results:")
-print(save_results.results_df)
-
-# Train the best model on the full training + validation set
-recommender_instance = ItemKNNCFRecommender(URM_train + URM_validation)
-recommender_instance.fit(**optuna_study.best_trial.params)
+    return optuna_study
