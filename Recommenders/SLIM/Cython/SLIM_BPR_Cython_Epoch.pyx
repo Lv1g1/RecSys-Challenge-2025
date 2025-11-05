d39619e1af275767db5315a8f5165363e07a0f9e
Lv1g1 <luigi.inguaggiato@gmail.com>
2025-11-05 12:28:55 +0100

    another fix

--- Recommenders/SLIM/Cython/SLIM_BPR_Cython_Epoch.pyx
+++ Recommenders/SLIM/Cython/SLIM_BPR_Cython_Epoch.pyx
@@ -530,7 +530,7 @@ cdef matrix_element_tree_s * pointer_new_matrix_element_tree_s(long column, floa
 
 
 # Functions to compare structs to be used in C qsort
-cdef int compare_struct_on_column(const void *a_input, const void *b_input):
+cdef int compare_struct_on_column(const void *a_input, const void *b_input) noexcept:
     """
     The function compares the column contained in the two struct passed.
     If a.column > b.column returns >0  
