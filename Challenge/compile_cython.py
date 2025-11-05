93e7c90dd3791a7d3ba488eafbd9849572d52ff7
Lv1g1 <luigi.inguaggiato@gmail.com>
2025-11-05 12:11:58 +0100

    added slim

--- Challenge/compile_cython.py
+++ Challenge/compile_cython.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
+usage = """
 Compile a given Cython source file in a subprocess
 
 Usage:
     python compile_script.py filename.pyx build_ext --inplace
+    python compile_script.py directory
 """
 
 from setuptools import setup, Extension
@@ -14,35 +15,68 @@ import numpy as np
 import sys
 import os
 
+if len(sys.argv) == 4:
+    fileToCompile = sys.argv[1]
 
-# Expect: script.py file.pyx build_ext --inplace  → 4 args
-if len(sys.argv) != 4:
-    raise ValueError(f"Wrong number of parameters. Expected 4, got {len(sys.argv)}")
+    if not fileToCompile.endswith(".pyx"):
+        raise ValueError("First argument must be a .pyx file")
 
-fileToCompile = sys.argv[1]
+    # Remove file argument before invoking setup()
+    del sys.argv[1]
 
-if not fileToCompile.endswith(".pyx"):
-    raise ValueError("First argument must be a .pyx file")
+    module_path = os.path.splitext(fileToCompile)[0]
+    extensionName = module_path.replace(os.sep, ".")
 
-# Remove file argument before invoking setup()
-del sys.argv[1]
+    extensions = [
+        Extension(
+            extensionName,
+            [fileToCompile],
+            extra_compile_args=["-O3"],  # Slightly better optimization
+            include_dirs=[np.get_include()],
+        )
+    ]
 
-module_path = os.path.splitext(fileToCompile)[0]
-extensionName = module_path.replace(os.sep, ".")
-
-extensions = [
-    Extension(
-        extensionName,
-        [fileToCompile],
-        extra_compile_args=["-O3"],  # Slightly better optimization
-        include_dirs=[np.get_include()],
+    setup(
+        name=extensionName,
+        ext_modules=cythonize(
+            extensions,
+            compiler_directives={"language_level": "3"},
+        ),
     )
-]
 
-setup(
-    name=extensionName,
-    ext_modules=cythonize(
-        extensions,
-        compiler_directives={"language_level": "3"},
-    ),
-)
+elif len(sys.argv) == 2:
+    directoryToCompile = sys.argv[1]
+
+    if not os.path.isdir(directoryToCompile):
+        raise ValueError("First argument must be a directory")
+
+    # Remove directory argument before invoking setup()
+    del sys.argv[1]
+
+    extensions = []
+    for root, _, files in os.walk(directoryToCompile):
+        for file in files:
+            if file.endswith(".pyx"):
+                filePath = os.path.join(root, file)
+                module_path = os.path.splitext(filePath)[0]
+                extensionName = module_path.replace(os.sep, ".")
+
+                extensions.append(
+                    Extension(
+                        extensionName,
+                        [filePath],
+                        extra_compile_args=["-O3"],  # Slightly better optimization
+                        include_dirs=[np.get_include()],
+                    )
+                )
+
+    setup(
+        name="cython_modules",
+        ext_modules=cythonize(
+            extensions,
+            compiler_directives={"language_level": "3"},
+        ),
+    )
+
+else:
+    raise ValueError(f"Wrong number of parameters." + usage)
