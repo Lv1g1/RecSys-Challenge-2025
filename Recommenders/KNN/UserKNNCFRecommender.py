0b7db48891ba7ba4448b0e08008542207bcaf390
Luigi Inguaggiato <luigi.inguaggiato@gmail.com>
2025-11-19 20:28:52 +0100

    Update BM25 parameters in UserKNNCFRecommender

--- Recommenders/KNN/UserKNNCFRecommender.py
+++ Recommenders/KNN/UserKNNCFRecommender.py
@@ -40,7 +40,7 @@ class UserKNNCFRecommender(BaseUserSimilarityMatrixRecommender):
 
         if feature_weighting == "BM25":
             self.URM_train = self.URM_train.astype(np.float32)
-            self.URM_train = okapi_BM_25(self.URM_train.T).T
+            self.URM_train = okapi_BM_25(self.URM_train.T, K1=similarity_args.get("BM25_k1", 1.2), B=similarity_args.get("BM25_b", 0.75)).T
             self.URM_train = check_matrix(self.URM_train, 'csr')
 
         elif feature_weighting == "TF-IDF":
